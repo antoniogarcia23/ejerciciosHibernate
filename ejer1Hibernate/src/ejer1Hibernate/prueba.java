3ccea49456c0e6a66076d99baa47037e83d56750
antonio <logost808@gmail.com>
2019-11-27 13:08:32 +0100

    primera subida

---
+++ ejer1Hibernate/src/ejer1Hibernate/prueba.java
@@ -0,0 +1,14 @@
+package ejer1Hibernate;
+
+public class prueba {
+
+	public prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ejer1Hibernate/src/ejer1Hibernate/utilidades.java
@@ -0,0 +1,9 @@
+package ejer1Hibernate;
+
+public class utilidades {
+
+	public utilidades() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
